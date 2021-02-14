905eb27452fae0b22f8fe2568f3410383570ac9c
ciontea <55166826+ciontea@users.noreply.github.com>
2021-02-14 00:24:26 -0500

    Create External Forwarding Outlook Rules.ps1

---
+++ Office 365/External Forwarding Outlook Rules.ps1
@@ -0,0 +1,40 @@
+#Purpose: Will find anyone with Outlook rules that auto forward emails to external users. Report will only run on Tuesday's but this can be removed
+
+$global:mailboxes              = Get-Mailbox -ResultSize Unlimited
+
+if ((Get-Date -Format "dddd") -like "*Tuesday*") {
+        foreach ($mailbox in $mailboxes) {
+                $forwardingRules = $null
+                Write-Host "Checking rules for $($mailbox.displayname) - $($mailbox.userprincipalname)" -foregroundColor Green
+                $rules = Get-InboxRule -Mailbox $mailbox.userprincipalname
+                $forwardingRules = $rules | Where-Object {$_.forwardto -or $_.forwardasattachmentto}
+                foreach ($rule in $forwardingRules) {
+                    $recipients = @()
+                    $recipients = $rule.ForwardTo | Where-Object {$_ -match "SMTP"}
+                    $recipients += $rule.ForwardAsAttachmentTo | Where-Object {$_ -match "SMTP"}
+                    $externalRecipients = @()
+                    foreach ($recipient in $recipients) {
+                        $email = ($recipient -split "SMTP:")[1].Trim("]")
+                        $domain = ($email -split "@")[1]
+                        if ($domains.DomainName -notcontains $domain) {
+                            $externalRecipients += $email
+                        }
+                    }
+                    if ($externalRecipients) {
+                        $extRecString = $externalRecipients -join ", "
+                        Write-Host "The Outlook rule '$($rule.Name)' forwards to $extRecString" -ForegroundColor Yellow
+
+                        $ruleHash = $null
+                        $ruleHash = [ordered]@{
+                            PrimarySmtpAddress = $mailbox.userprincipalname
+                            DisplayName        = $mailbox.DisplayName
+                            RuleId             = $rule.Identity
+                            RuleName           = $rule.Name
+                            RuleDescription    = $rule.Description
+                            ExternalRecipients = $extRecString
+                        }
+                        $ruleObject = New-Object PSObject -Property $ruleHash
+                        $ruleObject | Export-Csv "$logpath\ExternalRules-$date.csv" -NoTypeInformation -Append
+                    }
+    }
+}
